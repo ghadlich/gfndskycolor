7f705ae0446607e4ac9fe99028e4eb1c30ce8c8a
Grant Hadlich <ghadlich@gmail.com>
2021-09-24 14:48:32 -0500

    Application Hangs - UnhandledPromiseRejectionWarning: TimeoutError: Navigation timeout
    Adding a try catch block in case browser has issue.
    
    Fixes hang with error message:
    
    ```
    (node:238490) UnhandledPromiseRejectionWarning: TimeoutError: Navigation timeout of 30000 ms exceeded
        at Promise.then (/home/gfndskycolor/node_modules/puppeteer/lib/cjs/puppeteer/common/LifecycleWatcher.js:106:111)
    (node:238490) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 1)
    (node:238490) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
    ```

--- capture_image.js
+++ capture_image.js
@@ -17,15 +17,20 @@ function timeout(ms) {
       args: ["--window-size=1920,800"]
 
     })
-    const page = await browser.newPage();
-    //await page.setViewport({width: 5960, height: 1000})
-    await page.goto('https://www.youtube.com/watch?v=OEvLTpOKWDM');
-    await timeout(5000)
-    await page.keyboard.press('k');
-    await timeout(1000)
-    await page.keyboard.press('f');
-    await timeout(60000)
-    await page.screenshot({path: output_file});
-    browser.close();
 
+    try {
+      const page = await browser.newPage();
+      //await page.setViewport({width: 5960, height: 1000})
+      await page.goto('https://www.youtube.com/watch?v=OEvLTpOKWDM');
+      await timeout(5000)
+      await page.keyboard.press('k');
+      await timeout(1000)
+      await page.keyboard.press('f');
+      await timeout(60000)
+      await page.screenshot({path: output_file});
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await browser.close();
+    }
 })();
